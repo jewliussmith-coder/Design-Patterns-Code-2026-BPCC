246eacd41ad2b06909d03693316c964ad881682b
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-09 11:08:18 -0500

    Phase 3 file

--- Chapter4ProgProb/src/Enemy.java
+++ Chapter4ProgProb/src/Enemy.java
@@ -1,3 +1,12 @@
 abstract class Enemy {
+    protected Weapon weapon;
+    protected Armour armour;
+
+    public Enemy(EquipmentFactory equipmentFactory){
+        this.weapon = equipmentFactory.createWeapon();
+        this.armour = equipmentFactory.createArmour();
+    }
+
     public abstract void attack();
 }
+
