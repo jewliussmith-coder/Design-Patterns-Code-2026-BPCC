5e1e7942eee3584d7e5a992ecdf3174a3af3a8b9
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-09 11:14:57 -0500

    Phase 3 file

--- Chapter4ProgProb/src/Skeleton.java
+++ Chapter4ProgProb/src/Skeleton.java
@@ -1,7 +1,12 @@
 public class Skeleton extends Enemy{
 
+    public Skeleton(EquipmentFactory equipmentFactory){
+        super(equipmentFactory);
+    }
+
     @Override
     public void attack(){
-        System.out.println("Skeleton launches a Poison Spell!");
+        System.out.println("A Skeleton wearing a " + armour.getDescription()
+                + " attacks with a " + weapon.getDescription());
     }
 }
