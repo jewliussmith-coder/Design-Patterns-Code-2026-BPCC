273db95e885f0a92c489a6e9ef133c163802e1dc
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-09 11:14:29 -0500

    Phase 3 file

--- Chapter4ProgProb/src/SimpleEnemyFactory.java
+++ Chapter4ProgProb/src/SimpleEnemyFactory.java
@@ -2,9 +2,13 @@ public class SimpleEnemyFactory {
     public Enemy createEnemy(String type){
         switch (type){
             case "goblin":
-                return new Goblin();
+                return new Goblin(new WarriorEquipmentFactory());
             case "skeleton":
-                return new Skeleton();
+                return new Skeleton(new MageEquipmentFactory());
+            case "wolf":
+                return new Wolf(new WarriorEquipmentFactory());
+            case "slime":
+                return new Slime(new MageEquipmentFactory());
             default:
                 return null;
         }
