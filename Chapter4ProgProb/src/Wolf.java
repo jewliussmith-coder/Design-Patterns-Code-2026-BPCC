e20b156a15359e3522e049f5a3e13c7fdcd81271
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-09 11:15:58 -0500

    Phase 3 file

--- Chapter4ProgProb/src/Wolf.java
+++ Chapter4ProgProb/src/Wolf.java
@@ -1,6 +1,12 @@
 public class Wolf extends Enemy{
+    public Wolf(EquipmentFactory equipmentFactory){
+        super(equipmentFactory);
+    }
+
     @Override
     public void attack(){
-        System.out.println("Wolf unleashes a deadly howl!");
+        System.out.println("A Wolf wearing a " + armour.getDescription()
+                + " attacks with a " + weapon.getDescription());
     }
 }
+
