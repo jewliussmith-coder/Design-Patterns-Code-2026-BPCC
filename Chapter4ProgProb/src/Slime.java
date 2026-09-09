a68fe1ca4789422ebba50aa5b2486caeee1158d0
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-09 11:15:33 -0500

    Phase 3 file

--- Chapter4ProgProb/src/Slime.java
+++ Chapter4ProgProb/src/Slime.java
@@ -1,7 +1,13 @@
 public class Slime extends Enemy{
+    public Slime(EquipmentFactory equipmentFactory){
+        super(equipmentFactory);
+    }
+
     @Override
     public void attack(){
-        System.out.println("Slime envelops you and drains your life force");
+        System.out.println("A Slime wearing a " + armour.getDescription()
+                + " attacks with a " + weapon.getDescription());
     }
 
 }
+
