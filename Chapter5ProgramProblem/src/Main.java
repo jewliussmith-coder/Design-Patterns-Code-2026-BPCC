4d3c3864b35d7b05c3461d5f31c0ff1cc38cc9ee
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-15 11:58:23 -0500

    Phase 3 file

--- Chapter5ProgramProblem/src/Main.java
+++ Chapter5ProgramProblem/src/Main.java
@@ -2,25 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Before getting the ConfigurationManager...");
+        AudioSystem audioSystem = new AudioSystem();
+        GraphicsRenderer graphicsRenderer = new GraphicsRenderer();
+
+        audioSystem.playSound("Background Music");
+        graphicsRenderer.renderGame();
+
+        audioSystem.adjustVolume(40);
+
+        audioSystem.playSound("Battle Music");
+        graphicsRenderer.renderGame();
 
         ConfigurationManager config1 = ConfigurationManager.getInstance();
-
-        System.out.println("After first getInstance() call.");
-
         ConfigurationManager config2 = ConfigurationManager.getInstance();
 
-        System.out.println("After second getInstance() call.");
-
         System.out.println("Are both references the same object? " + (config1 == config2));
-
-        System.out.println("Original volume: " + config1.getVolume());
-
-        config1.setVolume(50);
-
-        System.out.println("Volume through config2: " + config2.getVolume());
-
-        System.out.println("Resolution: " + config1.getResolution());
-        System.out.println("Display Mode: " + config1.getDisplayMode());
+        System.out.println("Current volume: " + config2.getVolume());
     }
 }
