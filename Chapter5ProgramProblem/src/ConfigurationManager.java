07ac236794e70c3d89d51bac5b475b45d717985b
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-15 10:55:36 -0500

    Phase 1

---
+++ Chapter5ProgramProblem/src/ConfigurationManager.java
@@ -0,0 +1,46 @@
+public class ConfigurationManager {
+
+
+    private static final ConfigurationManager instance = new ConfigurationManager();
+
+
+    private int volume = 75;
+    private String resolution = "1920x1080";
+    private String displayMode = "Fullscreen";
+
+
+    private ConfigurationManager() {
+    }
+
+
+    public static ConfigurationManager getInstance() {
+        return instance;
+    }
+
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+
+    public String getDisplayMode() {
+        return displayMode;
+    }
+
+    public void setDisplayMode(String displayMode) {
+        this.displayMode = displayMode;
+    }
+}
---
+++ Chapter5ProgramProblem/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        ConfigurationManager config1 = ConfigurationManager.getInstance();
+        ConfigurationManager config2 = ConfigurationManager.getInstance();
+
+
+        System.out.println("Are both references the same object? " + (config1 == config2));
+
+
+        System.out.println("Original volume: " + config1.getVolume());
+
+
+        config1.setVolume(50);
+
+
+        System.out.println("Volume through config2: " + config2.getVolume());
+
+
+        System.out.println("Resolution: " + config1.getResolution());
+        System.out.println("Display Mode: " + config1.getDisplayMode());
+    }
+}
